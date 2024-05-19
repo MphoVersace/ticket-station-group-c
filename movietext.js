7f5d47bbf670cd085d2816b0964e8243b2df83b6
Thato Makhubela <makhubelathato@gmail.com>
2024-05-19 19:18:53 +0200

    updates on toggle and picture display function.

--- movietext.js
+++ movietext.js
@@ -1,10 +1,19 @@
 const arrows = document.querySelectorAll(".arrows");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow,movietext)=>{
-    arrows.addEventListener("click", ()=>{
-        movieLists[movietext].style.transform = translateX(${
+arrows.forEach((arrow,movietext) => {
+    const itemNumber =movielists[movietext].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrows.addEventListener("click", () => {
+        clickCounter++;
+        if(itemNumber -(4+clickCounter) >= 0) {
+        movieLists[movietext].style.transform = *translateX(${
             movieLists[movietext].computedStyleMap().get("transform")[0].x.value
-        -300}px);  
-    })
-})
+        -300}px)*;
+        } else{
+            movieLists[movIetext].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }  
+    });
+});
+
