0355074f29ba5e815c5a469d2fe52c684d9090c7
Thato Makhubela <makhubelathato@gmail.com>
2024-05-19 19:54:56 +0200

    Updates on Script

--- movietext.js
+++ movietext.js
@@ -5,8 +5,9 @@ arrows.forEach((arrow,movietext) => {
     const itemNumber =movielists[movietext].querySelectorAll("img").length;
     let clickCounter = 0;
     arrows.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270)
         clickCounter++;
-        if(itemNumber -(4+clickCounter) >= 0) {
+        if(itemNumber -(4+clickCounter) + (4+ratio) >= 0) {
         movieLists[movietext].style.transform = *translateX(${
             movieLists[movietext].computedStyleMap().get("transform")[0].x.value
         -300}px)*;
@@ -17,3 +18,14 @@ arrows.forEach((arrow,movietext) => {
     });
 });
 
+//toggle//
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,");
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+})
